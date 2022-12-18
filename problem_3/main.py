04c73106c5ec08b086f19b2c4e3e7eb0c063fa14
paraneus <anna.friedli@aviki.ch>
2022-12-18 18:27:07 +0100

    change problem 3

--- problem_3/main.py
+++ problem_3/main.py
@@ -4,15 +4,12 @@ from pprint import pprint
 
 def main():
     print(f"The name of this program is: {__name__}")
-# python program to print prime factors
  
 def primefactors(n):
-   #even number divisible
    while n % 2 == 0:
       print (2),
       n = n / 2
     
-   #n became odd
    for i in range(3,int(math.sqrt(n))+1,2):
      
       while (n % i == 0):
@@ -22,7 +19,7 @@ def primefactors(n):
    if n > 2:
       print (n)
  
-n = int(input("Enter the number for calculating the prime factors :\n"))
+n = int(input("prime factors :\n"))
 primefactors(n)
 
 
